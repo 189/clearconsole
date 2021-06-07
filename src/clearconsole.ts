8ea361b7cd95d189965dabbfc97317061fe04d0e
wangshangwen <wangshangwen>
2021-06-07 11:40:13 +0800

    version

--- src/clearconsole.ts
+++ src/clearconsole.ts
@@ -2,5 +2,5 @@
  * clear console
  */
 export default function clearConsole() {
-  process.stdout.write("\x1B[2J\x1B[0f");
+  process.stdout.write(process.platform === "win32" ? "\x1B[2J\x1B[0f" : "\x1B[2J\x1B[3J\x1B[H");
 }
