4aaacc7a847e4c1b7aa74a1153e74938c723d337
wangshangwen <wangshangwen>
2021-06-09 12:54:19 +0800

    feat: process.stdout

--- src/clearconsole.ts
+++ src/clearconsole.ts
@@ -3,5 +3,6 @@
  */
 export default function clearConsole() {
   const { stdout } = process;
+  console.log("ho~");
   stdout.write(process.platform === "win32" ? "\x1B[2J\x1B[0f" : "\x1B[2J\x1B[3J\x1B[H");
 }
