9745ebe2151e49b40c751f58fb6a1ee375645839
wangshangwen <wangshangwen>
2021-06-09 12:47:15 +0800

    refactor: process.stdout

--- src/clearconsole.ts
+++ src/clearconsole.ts
@@ -2,5 +2,6 @@
  * clear console
  */
 export default function clearConsole() {
-  process.stdout.write(process.platform === "win32" ? "\x1B[2J\x1B[0f" : "\x1B[2J\x1B[3J\x1B[H");
+  const { stdout } = process;
+  stdout.write(process.platform === "win32" ? "\x1B[2J\x1B[0f" : "\x1B[2J\x1B[3J\x1B[H");
 }
